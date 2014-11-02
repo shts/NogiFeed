6b345030bfe9001082e82be68cd9d9b0727a6b65
shts <shts.dev@gmail.com>
2014-11-02 16:52:39 +0900

    tiny fix

--- app/src/main/java/android/shts/jp/nogifeed/utils/PicassoHelper.java
+++ app/src/main/java/android/shts/jp/nogifeed/utils/PicassoHelper.java
@@ -6,9 +6,6 @@ import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
-/**
- * Created by saitoushouta on 2014/09/27.
- */
 public class PicassoHelper {
 
     private static final CircleTransformation CIRCLE_TRANSFORMATION = new CircleTransformation();
@@ -24,17 +21,6 @@ public class PicassoHelper {
                 .load(url)
                 .transform(CIRCLE_TRANSFORMATION)
                 .into(target);
-    }
-
-    public static void loadThumbnail(Context context, ImageView target, String url) {
-        Picasso.with(context)
-                .load(buildSiteThumbnailUrl(url))
-//                .placeholder(R.drawable.loading)
-                .into(target);
-    }
-
-    public static final String buildSiteThumbnailUrl(String linkUrl) {
-        return "http://capture.heartrails.com/256x256?" + linkUrl;
     }
 
 }
