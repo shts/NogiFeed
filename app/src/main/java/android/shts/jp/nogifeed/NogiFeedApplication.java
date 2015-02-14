aa98c3df70937a8ca64700249048343eed1ef09c
shts <shts.dev@gmail.com>
2015-02-15 01:25:17 +0900

    add push receive from Parse.com

--- app/src/main/java/android/shts/jp/nogifeed/NogiFeedApplication.java
+++ app/src/main/java/android/shts/jp/nogifeed/NogiFeedApplication.java
@@ -5,6 +5,8 @@ import android.content.Context;
 
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.Tracker;
+import com.parse.Parse;
+import com.parse.ParseInstallation;
 
 public class NogiFeedApplication extends Application {
 
@@ -15,5 +17,8 @@ public class NogiFeedApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Parse.initialize(this, getString(R.string.api_id), getString(R.string.api_key));
+        ParseInstallation.getCurrentInstallation().saveInBackground();
     }
+
 }
---
+++ app/src/main/java/android/shts/jp/nogifeed/receivers/FromParseReceiver.java
@@ -0,0 +1,32 @@
+package android.shts.jp.nogifeed.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class FromParseReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i("Parse", "onReceive");
+        try {
+            //jsonから値を取り出し
+            Bundle extra        = intent.getExtras();
+            String data         = extra.getString("com.parse.Data");
+            JSONObject json     = new JSONObject(data);
+            Log.i("Parse", data);
+
+            //取り出したデータを変数へ
+            String msg          = json.getString("msg");
+
+            Log.i("parse push test!", "msg : " + msg);
+
+        } catch (JSONException e) {
+            Log.e("failed to Parse : ", e.toString());
+        }
+    }
+}
