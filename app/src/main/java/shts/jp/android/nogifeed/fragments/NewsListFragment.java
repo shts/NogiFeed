8d362c5483e94b5bf281f16f2c3da8c76d083196
shts <shts.dev@gmail.com>
2016-01-11 20:40:57 +0900

    refactor

--- app/src/main/java/shts/jp/android/nogifeed/fragments/NewsListFragment.java
+++ app/src/main/java/shts/jp/android/nogifeed/fragments/NewsListFragment.java
@@ -1,13 +1,10 @@
 package shts.jp.android.nogifeed.fragments;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.ListFragment;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
