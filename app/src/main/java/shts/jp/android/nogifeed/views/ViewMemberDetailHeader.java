ebb36acec696b955253fae10b05ffb34d0b05b8b
shts <shts.dev@gmail.com>
2016-02-07 20:53:01 +0900

    fix NullPointerException

--- app/src/main/java/shts/jp/android/nogifeed/views/ViewMemberDetailHeader.java
+++ app/src/main/java/shts/jp/android/nogifeed/views/ViewMemberDetailHeader.java
@@ -2,7 +2,9 @@ package shts.jp.android.nogifeed.views;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
@@ -12,6 +14,8 @@ import android.widget.TextView;
 import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.squareup.otto.Subscribe;
+
+import java.util.List;
 
 import me.gujun.android.taggroup.TagGroup;
 import shts.jp.android.nogifeed.R;
@@ -74,19 +78,37 @@ public class ViewMemberDetailHeader extends LinearLayout {
             public void done(Member member, ParseException e) {
 
                 final Context context = getContext();
-                PicassoHelper.loadAndCircleTransform(
-                        context, profileImageView, member.getProfileImageUrl());
+                final String objectId = member.getObjectId();
 
+                if ("sdpSHPvZXg".equals(objectId) || "ojTXXVhx6n".equals(objectId) /* for debug */
+                        || "uJs1wH6AdB".equals(objectId) || "DAkpwgMk8w".equals(objectId)) {
+                    profileImageView.setImageResource(R.drawable.kensyusei);
+
+                    birthdayTextView.setVisibility(View.GONE);
+                    bloodTypeTextView.setVisibility(View.GONE);
+                    constellationTextView.setVisibility(View.GONE);
+                    heightTextView.setVisibility(View.GONE);
+                    tags.setVisibility(View.GONE);
+
+                } else {
+                    final String profileImageUrl = member.getProfileImageUrl();
+                    PicassoHelper.loadAndCircleTransform(
+                            context, profileImageView, profileImageUrl);
+
+                    nameSubTextView.setText(member.getNameSub());
+
+                    final Resources res = context.getResources();
+                    birthdayTextView.setText(res.getString(R.string.property_name_birthday, member.getBirthday()));
+                    bloodTypeTextView.setText(res.getString(R.string.property_name_blood_type, member.getBloodType()));
+                    constellationTextView.setText(res.getString(R.string.property_name_constellation, member.getConstellation()));
+                    heightTextView.setText(res.getString(R.string.property_name_height, member.getHeight()));
+
+                    List<String> statusList = member.getStatus();
+                    if (statusList != null && !statusList.isEmpty()) {
+                        tags.setTags(member.getStatus());
+                    }
+                }
                 nameMainTextView.setText(member.getNameMain());
-                nameSubTextView.setText(member.getNameSub());
-
-                final Resources res = context.getResources();
-                birthdayTextView.setText(res.getString(R.string.property_name_birthday, member.getBirthday()));
-                bloodTypeTextView.setText(res.getString(R.string.property_name_blood_type, member.getBloodType()));
-                constellationTextView.setText(res.getString(R.string.property_name_constellation, member.getConstellation()));
-                heightTextView.setText(res.getString(R.string.property_name_height, member.getHeight()));
-
-                tags.setTags(member.getStatus());
             }
         });
     }
