67e5eeb5c372db2c681b046c42148b56d3ea1e61
shts <shts.dev@gmail.com>
2016-05-21 12:39:06 +0900

    coordinate layout crash

--- app/src/main/java/shts/jp/android/nogifeed/activities/TopActivity.java
+++ app/src/main/java/shts/jp/android/nogifeed/activities/TopActivity.java
@@ -9,7 +9,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
 
